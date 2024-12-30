45e811893d089faec4cf2d610f69026392a613e2
arifulnahid <ainahidfsti@gmail.com>
2024-12-30 21:58:29 +0600

    remove

--- src/components/error/Error.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Error() {
-  return (
-    <div className='h-screen p-4 text-center'>Error</div>
-  )
-}
--- src/utils/totalPrice.js
+++
@@ -1,4 +0,0 @@
-export const calculateTotalPrice = (items = []) => {
-    let t = items.reduce((prev, cur) => prev+cur.item.price, 0)
-    return t
-}
